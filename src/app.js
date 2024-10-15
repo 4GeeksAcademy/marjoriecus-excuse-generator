1c3dcecac644e87975e427a2953e4e6177b3444d
Marjoriecus <101658277+Marjoriecus@users.noreply.github.com>
2024-10-15 18:57:38 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoArray = ["The dog", "My grandma", "The mailman", "My bird"];
+  let actionArray = ["ate", "peed", "crushed", "broke"];
+  let whatArray = ["my homework", "my phone", "the car"];
+  let whenArray = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let index1 = Math.floor(Math.random() * whoArray.length);
+  let index2 = Math.floor(Math.random() * actionArray.length);
+  let index3 = Math.floor(Math.random() * whatArray.length);
+  let index4 = Math.floor(Math.random() * whenArray.length);
+
+  let excuse =
+    whoArray[index1] +
+    " " +
+    actionArray[index2] +
+    " " +
+    whatArray[index3] +
+    " " +
+    whenArray[index4];
+
+  document.querySelector("#excuse").innerHTML = excuse;
 };
